f5991e02f8d7ed2eee6efbd111209e80f57a0556
emmanuelmacharia <samuelmarsha@outlook.com>
2019-06-26 12:35:51 +0300

    finishes creating a mock api using mock data; and get and delete data from the data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import numeral from 'numeral';
 //API IMPORT
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 //STYLES:
 import './index.css';
+// import { link } from 'fs';
 // import { get } from 'http';
 
 
@@ -19,12 +20,28 @@ getUsers().then(result => {
 
 	result.forEach(user => {
 		userBody += `<tr>
-		<td><a href="#" data-id= "${user.id}" class="deleteUser">Delete</a></td>
+		<td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
 		<td>${user.id}</td>
 		<td>${user.firstName}</td>
 		<td>${user.lastName}</td>
 		<td>${user.email}</td>
 		</tr>`
 	});
-	global.document.getElementById('users').innerHTML = userBody;
+	document.getElementById('users').innerHTML = userBody;
+
+	const deleteLinks = document.getElementsByClassName("deleteUser");
+
+	//Must use array.from to create a real array from the DOM collection
+	//getElementsByClassname returns an array-like object
+	Array.from(deleteLinks, link => {
+		link.onclick = function (event) {
+			const element = event.target;
+			event.preventDefault();
+			deleteUser(element.attributes["data-id"].value);
+			const row = element.parentNode.parentNode;
+			row.parentNode.removeChild(row);
+		};
+	});
 })
+
+
